478d4c81c18b8b38c5bb8ce80129f01ea700a9e5
ArjunaPatil <30894586+ArjunaPatil@users.noreply.github.com>
2017-09-16 17:07:06 +0530

    Update masterlocking.py

--- team/masterlocking.py
+++ team/masterlocking.py
@@ -13,66 +13,70 @@ def lock_master_forms(employee):
     lock_doc=0
     lock_che=0
     
+    t_obj1=0
+    t_obj2=0
+    t_obj=0
+    
+    t_drc1=0
+    t_drc2=0
+    t_drc=0
+    
+    t_chc1=0
+    t_chc2=0
+    t_chc=0
+    
+    t_cmc1=0
+    t_cmc2=0
+    t_cmc=0
+    
+    today_date = frappe.utils.data.get_datetime().strftime('%Y/%m/%d')
+    current_time = local_time()
+    
+    
     lock_pro = frappe.db.sql("""select m_pro from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
     lock_pat = frappe.db.sql(""" select m_pat from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
     lock_doc = frappe.db.sql(""" select m_doc from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
     lock_che = frappe.db.sql(""" select m_che from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
     
+    frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_obj1,'')as obj_frm_date,ifnull(t_obj2,'')as obj_to_date,ifnull(t_obj_time,'')as obj_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
+    
+    frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_drc1,'')as doc_frm_date,ifnull(t_drc2,'')as doc_to_date,ifnull(t_drc_time,'')as doc_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
+    
+    frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_chc1,'')as che_frm_date,ifnull(t_chc2,'')as che_to_date,ifnull(t_chc_time,'')as che_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
+    
+    frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_cmc1,'')as cam_frm_date,ifnull(t_cmc2,'')as cam_to_date,ifnull(t_cmc_time,'')as cam_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
+    
     dict = {'lock_pro': '',
             'lock_pat': '',
             'lock_doc':'',
             'lock_che': '',
+            
+            'today_date': '',
+            'current_time': '',
+            
             'lock_T_Obj': '',
             'lock_T_DrC': '',
             'lock_T_ChC': '',
-            'lock_T_CmC': ''   
+            'lock_T_CmC': '',
+            'lock_T_Obj': '',
+            'lock_T_DrC': '',
+            'lock_T_ChC': '',
+            'lock_T_CmC': '',
+            'lock_T_Obj': '',
+            'lock_T_DrC': '',
+            'lock_T_ChC': '',
+            'lock_T_CmC': ''
            }
     
-    dict['cnt_emp_objective'] = lock_pro[0].m_pro;
-    dict['cnt_of_emp_dcr'] = lock_pat[0].m_pat;
-    dict['cnt_of_emp_chem'] = lock_doc[0].m_doc;
-    dict['cnt_of_emp_camp'] = lock_che[0].m_che;
+    dict['lock_pro'] = lock_pro[0].m_pro;
+    dict['lock_pat'] = lock_pat[0].m_pat;
+    dict['lock_doc'] = lock_doc[0].m_doc;
+    dict['lock_che'] = lock_che[0].m_che;
+    dict['lock_che'] = lock_che[0].m_che;
+    dict['lock_che'] = lock_che[0].m_che;
     
     return dict
-    
-    
-    
-def lock_transaction_forms(employee,formname,date):
-    lock_flag=0
-    frmdate=""
-    todate=""
-    locktime=""
-    today_date = frappe.utils.data.get_datetime().strftime('%Y/%m/%d')
-    current_time = local_time()
-    if(formname == 'T_Obj'):
-        frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_obj1,'')as obj_frm_date,ifnull(t_obj2,'')as obj_to_date,ifnull(t_obj_time,'')as obj_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
-
-    elif formname == "T_DrC":
-        frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_drc1,'')as doc_frm_date,ifnull(t_drc2,'')as doc_to_date,ifnull(t_drc_time,'')as doc_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
-
-    elif formname == "T_ChC":
-        frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_chc1,'')as che_frm_date,ifnull(t_chc2,'')as che_to_date,ifnull(t_chc_time,'')as che_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
-
-    elif formname == "T_CmC":
-        frmdate,todate,locktime = frappe.db.sql(""" select ifnull(t_cmc1,'')as cam_frm_date,ifnull(t_cmc2,'')as cam_to_date,ifnull(t_cmc_time,'')as cam_time from 1bd3e0294da19198.`tabUser` where name= {0} """.format(employee), as_dict=1)
-
-    else:
-        lock_flag=0
-
-    if frmdate != "" and todate != "" and locktime != "":
-        if(date >= frmdate and date <= todate):
-            lock_flag=1
-        elif(today_date == date):
-            if current_time<=locktime:
-                lock_flag=1
-            else:
-                lock_flag=0
-        else:
-            lock_flag=0
-    else:
-        lock_flag=0
-
-    return lock_flag
+         
 
 #Europe/Berlin
 def local_time(zone='Asia/Kolkata'):
